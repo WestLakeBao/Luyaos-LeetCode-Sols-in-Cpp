05cada2eb7c2286accd03dce941e27cca4cf67d3
chenluyao <chenluyao1020@gmail.com>
2026-02-23 23:23:28 +0000

    28

--- 028. Implement strStr().cpp
+++ 028. Implement strStr().cpp
@@ -5,19 +5,8 @@ using namespace std;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if (haystack.length()<needle.size()) return -1;
-        if (haystack.length()==1) {
-            if (haystack[0] == needle[0] || needle.length() == 0) return 0;
-            else return -1;
-        }
-        if (haystack.length()==0 && needle.length()==0) return 0;
-        int left=0;
-        while (left<haystack.size()){
-            if (haystack.substr(left, needle.length())==needle)
-                return left;
-            else {
-                left += 1;
-            }
+        for (int i=0, j=needle.length(); i+j<=haystack.length(); i++){
+            if (haystack.substr(i, j)==needle) return i;
         }
         return -1;
     }
