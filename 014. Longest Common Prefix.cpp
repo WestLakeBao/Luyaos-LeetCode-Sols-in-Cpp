0bd818a2b3b93a0041a435d2ea2bcce3c4a1656d
chenluyao <chenluyao1020@gmail.com>
2026-02-23 22:59:46 +0000

    14

--- 014. Longest Common Prefix.cpp
+++ 014. Longest Common Prefix.cpp
@@ -6,22 +6,15 @@ using namespace std;
 
 class Solution {
 public:
-    string longestCommonPrefix(vector<string> strs) {
-        if (strs.size() == 0) return "";
-        if (strs.size() == 1) return strs[0];
-        int shortestLength = INT_MAX;
-        string result;
-//        for (int i=0; i<strs.size(); i++){
-//            if (strs[i].size() < shortestLength){
-//                shortestLength = strs[i].size();
-//            }
-//        }
+    string longestCommonPrefix(vector<string>& strs) {
+        string common;
         for (int i=0; i<strs[0].size(); i++){
             for (int j=1; j<strs.size(); j++){
-                if (i>=strs[j].size() || strs[j][i] != strs[0][i]) return result;
+                if (i >= strs[j].size() | strs[0][i]!=strs[j][i]){
+                    return common;
+                }
             }
-            result += strs[0][i];
+            common += strs[0][i];
         }
-        return result;
+        return common;
     }
-};
